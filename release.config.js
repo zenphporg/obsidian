3b29ff08c2440c7b8ed7f858441a1675212cf164
secondman <secondman@jetstreamlabs.com>
2025-11-21 14:11:07 -0700

    chore: fix release config immutable error
    Signed-off-by: secondman <secondman@jetstreamlabs.com>

--- release.config.js
+++ release.config.js
@@ -37,36 +37,45 @@ const releaseNotesGeneratorOptions = {
         test: 'Code Testing',
       };
 
-      commit.type = types[commit.type];
+      // Create a new object to avoid modifying the immutable commit
+      const modifiedCommit = Object.assign({}, commit);
 
-      if (typeof commit.hash === 'string') {
-        commit.shortHash = commit.hash.substring(0, 7);
+      // Map the type
+      if (types[modifiedCommit.type]) {
+        modifiedCommit.type = types[modifiedCommit.type];
       }
 
-      if (typeof commit.subject === 'string') {
+      if (typeof modifiedCommit.hash === 'string') {
+        modifiedCommit.shortHash = modifiedCommit.hash.substring(0, 7);
+      }
+
+      if (typeof modifiedCommit.subject === 'string') {
         let url = context.repository ? `${context.host}/${context.owner}/${context.repository}` : context.repoUrl;
         if (url) {
           url = `${url}/issues/`;
           // Issue URLs.
-          commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
+          modifiedCommit.subject = modifiedCommit.subject.replace(/#([0-9]+)/g, (_, issue) => {
             issues.push(issue);
             return `[#${issue}](${url}${issue})`;
           });
         }
         if (context.host) {
           // User URLs.
-          commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
-            if (username.includes('/')) {
-              return `@${username}`;
-            }
+          modifiedCommit.subject = modifiedCommit.subject.replace(
+            /\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g,
+            (_, username) => {
+              if (username.includes('/')) {
+                return `@${username}`;
+              }
 
-            return `[@${username}](${context.host}/${username})`;
-          });
+              return `[@${username}](${context.host}/${username})`;
+            }
+          );
         }
       }
 
       // remove references that already appear in the subject
-      commit.references = commit.references.filter((reference) => {
+      modifiedCommit.references = modifiedCommit.references.filter((reference) => {
         if (issues.indexOf(reference.issue) === -1) {
           return true;
         }
@@ -74,7 +83,7 @@ const releaseNotesGeneratorOptions = {
         return false;
       });
 
-      return commit;
+      return modifiedCommit;
     },
   },
 };
